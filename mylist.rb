80eb672bcd07d81d78991ea0e75899df4dcbfc3a
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-09-22 14:53:46 +0200

    Add list

---
+++ mylist.rb
@@ -0,0 +1,24 @@
+class MyList
+  include MyEnumerable
+
+  def initialize(*args)
+    @list = args
+  end
+
+  def each
+    @list.each { |item| yield item }
+  end
+end
+
+list = MyList.new(1, 2, 3, 4)
+p list.all? {|e| e < 5}
+p list.all? {|e| e > 5}
+
+puts
+
+p list.any? {|e| e == 2}
+p list.any? {|e| e == 5}
+
+puts
+
+p list.filter {|e| e.even?}
