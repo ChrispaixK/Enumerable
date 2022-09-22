76af5604c9c4051cb37840a7a6735002d33d4c6b
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-09-22 16:08:50 +0200

    Fix linters errors

--- mylist.rb
+++ mylist.rb
@@ -14,14 +14,14 @@ class MyList
 end
 
 list = MyList.new(1, 2, 3, 4)
-p list.all? { ( |e| e < 5 ) }
-p list.all? { ( |e| e > 5 ) }
+p(list.all? { |e| e < 5 })
+p(list.all? { |e| e > 5 })
 
 puts
 
-p list.any? { ( |e| e == 2 ) }
-p list.any? { ( |e| e == 5 ) }
+p(list.any? { |e| e == 2 })
+p(list.any? { |e| e == 5 })
 
 puts
 
-p list.filter { ( |e| e.even? ) }
+p(list.filter(&:even?))
